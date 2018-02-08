88dbbe4ebc26e4419dd823038f3cf7da3982d7d6
Justin Johnson <Justin.D.Johnson@hotmail.com>
2018-02-08 17:19:36 -0500

    test sql

---
+++ testRabbitMQServer.php
@@ -0,0 +1,60 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function doLogin($username,$password)
+{
+    $con = mysqli_connect ("localhost", "root", "", "userdata");// or die("Could not connect: " . mysql_error());
+    
+    // Check connection
+    if (mysqli_connect_errno())
+    {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    // $db = mysql_select_db ("root") or die("No database.");
+    //session_start();
+
+    // lookup username in databas
+    // check password
+    
+    $query = "SELECT * FROM logininfo WHERE username = '$username' and pword = '$password'";
+    
+    if ($result = mysqli_query($con, $query))
+    {
+      //session_start();
+      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+      $sessionid = $row["id"];  // Initializing Session with value of PHP Variable
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return doLogin($request['username'], $request['password']);
+    case "validate_session":
+      return doValidate($request['sessionid']);
+  }
+  return array("returnCode" => '0', 'message'=>"sessionid " . sessionid . "");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+$server->process_requests('requestProcessor');
+exit();
+?>
+
