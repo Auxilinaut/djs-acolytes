098b6e5c823a3634b58b8f0d1dd5468f956622d1
Justin Johnson <Justin.D.Johnson@hotmail.com>
2018-02-14 17:45:38 -0500

    db test

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -6,12 +6,13 @@
 
   function doLogin($username, $password)
   {
+    echo "trying to connect to mysql server" . PHP_EOL;
     $con = mysqli_connect ("localhost", "root", "", "userdata");// or die("Could not connect: " . mysql_error());
     
     // Check connection
     if (mysqli_connect_errno())
     {
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      echo "Failed to connect to MySQL: " . mysqli_connect_error() . PHP_EOL;
     }
     // $db = mysql_select_db ("root") or die("No database.");
     //session_start();
@@ -19,17 +20,20 @@
     // lookup username in databas
     // check password
     
+    echo "query consists of username " . $username . " and password " . $password;
     $query = "SELECT * FROM logininfo WHERE username = '$username' and pword = '$password'";
     
     if ($result = mysqli_query($con, $query))
     {
       //session_start();
       $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-      $sessionid = $row["id"];  // Initializing Session with value of PHP Variable
+      $sessionid = $row[id];  // Initializing Session with value of PHP Variable
+      echo "sessionid: " . $sessionid . PHP_EOL;
       return true;
     }
     else
     {
+      echo "no sessionid";
       return false;
     }
   }
