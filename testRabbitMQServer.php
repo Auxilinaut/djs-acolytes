0a4bbefa47c54331923024b3102c1869e0e207a8
Justin Johnson <Justin.D.Johnson@hotmail.com>
2018-02-14 19:49:12 -0500

    global sessionid var still globalizing

--- testRabbitMQServer.php
+++ testRabbitMQServer.php
@@ -31,7 +31,8 @@
     {
       //session_start();
       $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-      global $sessionid = $row["id"];  // Initializing Session with value of PHP Variable
+      global $sessionid; 
+      $sessionid = $row["id"];  // Initializing Session with value of PHP Variable
       echo "sessionid: " . $sessionid . PHP_EOL;
       return true;
     }
