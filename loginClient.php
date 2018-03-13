b78f964d73a960e311cf3e59bde89d12e4095238
Justin Johnson <jj246@njit.edu>
2018-03-12 21:50:36 -0400

    cleanup

--- loginClient.php
+++ loginClient.php
@@ -7,18 +7,18 @@ require_once('rabbitMQLib.inc');
 
 //echo "we are about to connect" . PHP_EOL;
 
-$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+$client = new rabbitMQClient("loginRabbitMQ.ini", "testServer");
 
 //echo "we made client" . PHP_EOL;
 
 if (isset($argv[1]))
 {
   $msg = $argv[1];
-}
+}/*
 else
 {
   $msg = "test message";
-}
+}*/
 
 $request = array();
 $request['type'] = "login";
@@ -32,8 +32,10 @@ $request['password'] = $_POST["pword"];
 //echo "              request uname " . $request['username'] . PHP_EOL;
 //echo "              request pword " . $request['password'] . PHP_EOL;
 
-
-$request['message'] = $msg;
+if (isset($msg))
+{
+  $request['message'] = $msg;
+}
 $response = $client->send_request($request);
 //$response = $client->publish($request);
 
